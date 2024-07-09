9d65f2ec3d91103b59996a91951efa4462d69100
jonas.martinez <cbobbxta.fr@gmail.com>
2024-07-09 11:01:26 +0200

    metrics

--- src/views/lenra/home.js
+++ src/views/lenra/home.js
@@ -5,11 +5,6 @@ export default function (_data, _props) {
     return Flex([
         View("lenra.counter")
             .find(Counter, {
-                user: "@me"
-            })
-            .props({ text: "My personnal counter" }),
-        View("lenra.counter")
-            .find(Counter, {
                 user: "global"
             })
             .props({ text: "The common counter" }),
